a3c8f91237480730319910201e5dcdd5a7195a25
Ícaro Emannuel <emannuelalmeida@gmail.com>
2021-01-29 22:36:32 -0300

    Menu graphics added

---
+++ GameJam2021/Assets/SelectOptionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectOptionScript : MonoBehaviour
+{
+    
+    enum MenuOption { Start, Load, Options, Credits, Quit}
+    MenuOption option = 0;
+    GameObject menuSelector = GameObject.Find("Menu Selector"); 
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            option--;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            option++;
+
+        if (option < 0)
+            option = MenuOption.Quit;
+        else if ((int)option > 3)
+            option = MenuOption.Start;
+    }
+}
