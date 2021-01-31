23af5f416b335f785ee41519536294efd6c37bec
Jorge David Muffareg Junior <444.j.444@gmail.com>
2021-01-30 23:42:17 -0300

    Updated External Scene
    Updated External  Scene with new sprites and a new Transition Script.

---
+++ GameJam2021/Assets/Scripts/TransitionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        other.gameObject.transform.position = new Vector3(-28.44f, -2.7f, 0);
+    }
+}
